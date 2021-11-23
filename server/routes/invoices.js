f17f3200215bfdefdd3b8a02456bfd53de104e09
ngocquang2509 <ngocnqgcd18410@fpt.edu.vn>
2021-11-23 23:27:14 +0700

    route client invoice profile

---
+++ server/routes/invoices.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import { getInvoices, createInvoice, updateInvoice, deleteInvoice, getInvoice, getInvoicesByUser } from '../controllers/invoices.js'
+
+const router = express.Router()
+
+router.get('/:id', getInvoice)
+router.get('/', getInvoicesByUser)
+router.post('/', createInvoice)
+router.patch('/:id', updateInvoice)
+router.delete('/:id', deleteInvoice)
+
+
+export default router
