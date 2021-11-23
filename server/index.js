fd2f07cf33723a4fa19eec048a85fb9368b57eae
ngocquang2509 <ngocnqgcd18410@fpt.edu.vn>
2021-11-23 23:29:37 +0700

    ..

--- server/index.js
+++ server/index.js
@@ -17,6 +17,9 @@ const DB_URL = process.env.DB_URL
 const PORT = process.env.PORT || 3001
 
 app.use('/users', userRoutes)
+app.use('/profiles', profile)
+app.use('/clients', clientRoutes)
+app.use('/invoices', invoiceRoutes)
 
 mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
